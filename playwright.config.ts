c8084265b6f8bc5a7d9cc013d9f24840eba8f0ee
ore88ore <dev.ore88ore@gmail.com>
2024-07-15 16:27:14 +0900

    chore: 実行ブラウザを変更

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,10 +33,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
     //
     // {
     //   name: "firefox",
@@ -67,10 +67,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome', headless: false },
-    },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome', headless: false },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
